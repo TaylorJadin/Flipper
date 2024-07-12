36be96f41d574b371acd38c189aa4013e5b6c6e0
Taylor Jadin <jadin.taylor@gmail.com>
2024-07-12 14:08:02 -0500

    sync del

--- flipperSync.sh
+++ flipperSync.sh
@@ -13,7 +13,7 @@ fi
 # Check if run from root of SD Card
 if [ -f "Manifest" ]; then
     mkdir -p Flipper
-    rsync -ahP --exclude '.git*' --exclude 'flipperSync.sh' --exclude '*.mp4' --exclude '*.png' --exclude '*.jp*' $flipper_repo/ Flipper/
+    rsync -ahP --delete --exclude '.git*' --exclude 'flipperSync.sh' --exclude '*.mp4' --exclude '*.png' --exclude '*.jp*' $flipper_repo/ Flipper/
 else
     echo "Please run this script from the root of the SD card like this: ./flipperSync.sh"
     exit
