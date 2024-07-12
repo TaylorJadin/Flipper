b25d9d7f6a9f88bbe266a70f777327cb912b8ad1
Taylor Jadin <jadin.taylor@gmail.com>
2024-07-12 14:06:54 -0500

    sync script

--- flipperSync.sh
+++ flipperSync.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-flipper_repo=$github/Flipper
+flipper_repo=~/Documents/GitHub/Flipper
 
 # Exit if flipper repo path doesn't check out
-if ![ -f "$flipper_repo/flipperSync.sh" ]; then
+if [ -f "$flipper_repo/flipperSync.sh" ]; then
+    echo "Flipper repo found at $flipper_repo"
+else
   echo "Flipper repo not found"
   exit
 fi
 
 # Check if run from root of SD Card
-if [ -f "Manifest" ]; then\
+if [ -f "Manifest" ]; then
     mkdir -p Flipper
-    rsync -ahP --exclude '.git' --exclude 'flipperSync.sh' --exclude '*.mp4' --exclude '*.png' --exclude '*.jp*' $flipper_repo/ .Flipper/
+    rsync -ahP --exclude '.git*' --exclude 'flipperSync.sh' --exclude '*.mp4' --exclude '*.png' --exclude '*.jp*' $flipper_repo/ Flipper/
 else
-    echo "Please run this script from the root of the SD card"
+    echo "Please run this script from the root of the SD card like this: ./flipperSync.sh"
     exit
 fi
